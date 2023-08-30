299e02485c53300cac8982584c9d2e0257e46a78
Azall <90371310+Arxzll@users.noreply.github.com>
2023-08-30 10:53:52 +0700

    Add files via upload

---
+++ Pengaduan Masyarakat/admin/tampilan.php
@@ -0,0 +1,50 @@
+<?php 
+	// cek apakah yang mengakses halaman ini sudah login
+	session_start();
+if($_SESSION['level']=="masyarakat"){
+header("Location:../Halaman Utama/tampilan");
+}
+if(!isset($_SESSION)){
+    header("Location:../login/login.php");
+}
+ 
+	?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=, initial-scale=1.0">
+  <title>Document</title>
+ 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="assyu.css">
+  <body>
+    <!-- As a heading -->
+    <nav class="navbar navbar-dark d-flex justify-content-center"style="border: 1px solid rgba(65, 53, 67, 0.40);
+    background: #343334;">
+      <div class="container-fluid">
+        <div class="navbar-brand mb-0 ">Pengaduan Masyarakat</div>
+      </div>
+    </nav>
+
+       <div class="d-flex">
+        <div class="d-flex align-items-start" style="width: 120px;
+        height: 90vh;
+        border-right: 1px solid #343334;
+    background: rgba(49, 46, 50, 0.85); opacity:0.9px;">
+            <div class="nav flex-column nav-pills me-3 text" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+              <a class="nav-link" href="tampilan.php">Home</a>
+              <a class="nav-link" href="laporan.php">Laporan</a>
+              <a class="nav-link" href="../logout.php">Log out</a>
+            </div>
+          </div>
+          <div class="container" style="width: 100%;">
+              <h3 class="text-center mt-4" style="color: aliceblue;">Layanan Aspirasi dan Pengaduan Online Rakyat
+              </h3>
+              <h4 class="text-center" style="color: aliceblue;">Sampaikan laporan Anda langsung kepada instansi pemerintah berwenang</h4>
+            </div>
+
+          </div>
+         <script src="./bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ Pengaduan Masyarakat/tanggapan.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 30, 2023 at 05:50 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pengaduan_masyarakat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tanggapan`
+--
+
+CREATE TABLE `tanggapan` (
+  `id_tanggapan` int(11) NOT NULL,
+  `id_pengaduan` int(11) NOT NULL,
+  `tgl_tanggapan` date NOT NULL,
+  `tanggapan` text NOT NULL,
+  `id_petugas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tanggapan`
+--
+
+INSERT INTO `tanggapan` (`id_tanggapan`, `id_pengaduan`, `tgl_tanggapan`, `tanggapan`, `id_petugas`) VALUES
+(0, 1, '2023-08-30', 'sadsadsadasddas', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tanggapan`
+--
+ALTER TABLE `tanggapan`
+  ADD PRIMARY KEY (`id_tanggapan`),
+  ADD KEY `id_pengaduan` (`id_pengaduan`),
+  ADD KEY `id_petugas` (`id_petugas`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tanggapan`
+--
+ALTER TABLE `tanggapan`
+  ADD CONSTRAINT `tanggapan_ibfk_2` FOREIGN KEY (`id_pengaduan`) REFERENCES `pengaduan` (`id_pengaduan`),
+  ADD CONSTRAINT `tanggapan_ibfk_3` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
